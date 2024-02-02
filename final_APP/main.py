ce06e83a6a1cfb3c76555e68043fac3c510cf465
oscarBarEs <oscarbarquillaesteban@gmail.com>
2024-02-02 13:34:57 +0100

    added header and changed set_scalars, remove TrameApp

--- final_APP/main.py
+++ final_APP/main.py
@@ -1,112 +1,71 @@
-from trame.app import get_server
-from trame.ui.vuetify2 import SinglePageWithDrawerLayout
-from trame.widgets import html
-from trame.widgets import vuetify2 as vuetify
-from trame.widgets import vtk,  plotly
-from trame.decorators import TrameApp, change, life_cycle
+"""
+Control Scalar Array
+~~~~~~~~~~~~~~~~~~~~
+
+Extending our simple example to have a dropdown menu to control which
+scalar array is used to color the mesh.
+"""
 
 import pyvista as pv
+from pyvista import examples
 from pyvista.trame.ui import plotter_ui
+from trame.app import get_server
+from trame.ui.vuetify2 import SinglePageWithDrawerLayout
+from trame.widgets import vuetify
 
-
-# noinspection PyUnresolvedReferences
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkCommonDataModel import vtkDataObject
-
-from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkDataSetMapper,
-    vtkPolyDataMapper,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer
-)
-#Parts of the APP
-from components.page.header import header
-#Table
-from components.fromJsonToTable import fetch_data
 from components.tableOfSimulation import table_of_simulation
+from components.page.header import header
 
-from components.graphs import plotly_graph_vuetify
-#Read Mesh
-from components.meshReader import readVTKMesh
-
-
-# Required for rendering initialization, not necessary for
-# local rendering, but doesn't hurt to include it
-# -----------------------------------------------------------------------------
-# Globals
-# -----------------------------------------------------------------------------
-
-DEFAULT_RESOLUTION = 6
 pv.OFF_SCREEN = True
-pl = pv.Plotter()
-# -----------------------------------------------------------------------------
-# VTK pipeline
-# -----------------------------------------------------------------------------
-#actor = readMesh() # en meshReader.py
 
+server = get_server()
+state, ctrl = server.state, server.controller
+
+#mesh = examples.download_antarctica_velocity()
 mesh = pv.read("Resources/info/ventricle_Tagged.vtk")
+
+pl = pv.Plotter()
 actor = pl.add_mesh(mesh)
 pl.view_xy()
 
-# Mesh: Setup default representation to surface
-"""    @change("scalars")
-    def set_scalars(scalars=mesh.active_scalars_name, **kwargs):
-        actor.mapper.array_name = scalars
-        actor.mapper.scalar_range = mesh.get_data_range(scalars)
-        ctrl.view_update()"""     
-# -----------------------------------------------------------------------------
-# Main Page
-# -----------------------------------------------------------------------------
-@TrameApp()
-class App_Hearth_Helper:
 
-    def __init__(self, server=None,ctrl=None, table_size=10):
-        self.server = get_server(server, client_type="vue2")
-        self.ctrl = self.server.controller
-        self.state = self.server.state
-        self.ui = self._build_ui()
-
-    @life_cycle.server_reload
-    @life_cycle.server_ready
-    def reload_ui(self, *args, **kwargs):
-        self.ui = self._build_ui()
-    @change("scalars")
-    def set_scalars(scalars=mesh.active_scalars_name, **kwargs):
-        actor.mapper.array_name = scalars
-        actor.mapper.scalar_range = mesh.get_data_range(scalars)
-        ctrl.view_update()
+@state.change("scalars")
+def set_scalars(scalars=mesh.active_scalars_name, **kwargs):
+    actor.mapper.array_name = scalars
+    actor.mapper.scalar_range = mesh.get_data_range(scalars)
+    ctrl.view_update()
 
 
-    def _build_ui(self):
-        with SinglePageWithDrawerLayout(self.server) as layout:
-            layout.title.set_text("Heart Helper")
-
-            with layout.content:
-                with vuetify.VContainer(
-                    fluid=True,
-                    classes="pa-0 fill-height",
-                    
-                ):
-                    with vuetify.VContainer(
-                        fluid=True,
-                        classes="pa-0 fill-height",
-                        height="100%",
-                    ):
-                        #plotly_graph_vuetify()
-                        view = plotter_ui(pl)
-                        self.ctrl.view_update = view.update                
-                #self.ctrl.on_server_ready.add(view.update)
+@state.change("log_scale")
+def set_log_scale(log_scale=False, **kwargs):
+    actor.mapper.lookup_table.log_scale = log_scale
+    ctrl.view_update()
 
 
-            with layout.toolbar:
-                header()
-                    
+with SinglePageWithDrawerLayout(server) as layout:
+    with layout.toolbar:
+        header()
+
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height",
+        ):
+            # Use PyVista UI template for Plotters
+            view = plotter_ui(pl)
+            ctrl.view_update = view.update
             with layout.drawer as drawer:
                 drawer.width = "40%"
                 vuetify.VDivider(classes="mb-2")
                 with vuetify.VCard():
+                    vuetify.VCheckbox(
+                        label="Log Scale",
+                        v_model=("log_scale", False),
+                        hide_details=True,
+                        dense=True,
+                        outlined=True,
+                    )
                     vuetify.VSelect(
                         label="Scalars",
                         v_model=("scalars", mesh.active_scalars_name),
@@ -120,15 +79,13 @@ class App_Hearth_Helper:
                     vuetify.VDataTable(**table_of_simulation('output.json'))
                     vuetify.VCardText(children=["This is a heart mesh"])
 
-        return layout
+server.start()
 
-# -----------------------------------------------------------------------------
-# Main
-# -----------------------------------------------------------------------------
-def main():
-    app = App_Hearth_Helper()
-    app.server.start()
-
-if __name__ == "__main__":
-    # server.start()
-    main()
+###############################################################################
+# .. raw:: html
+#
+#     <center>
+#       <a target="_blank" href="https://colab.research.google.com/github/pyvista/pyvista-tutorial/blob/gh-pages/notebooks/tutorial/09_trame/c_trame_scalars.ipynb">
+#         <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/ width="150px">
+#       </a>
+#     </center>
